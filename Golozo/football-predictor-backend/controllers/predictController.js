6880a37f7641671d7c38092a3fdbfb4699281c8d
Mesh1928 <w1994512@westminster.ac.uk>
2026-04-22 21:32:56 +0100

    Fix SQL query syntax

--- Golozo/football-predictor-backend/controllers/predictController.js
+++ Golozo/football-predictor-backend/controllers/predictController.js
@@ -17,7 +17,7 @@ db.connect((err) => {
 });
 
 const getPrediction = (req, res) => {
-    const query = 'SELECT team, opponent, date, predicted_target FROM matches WHERE venue_code = 1 AND date >= ''2026-04-22'' ORDER BY date ASC LIMIT 20';
+    const query = "SELECT team, opponent, date, predicted_target FROM matches WHERE venue_code = 1 AND date >= '2026-04-22' ORDER BY date ASC LIMIT 20";
     db.query(query, (err, results) => {
         if (err) {
             console.error('Query error:', err);
